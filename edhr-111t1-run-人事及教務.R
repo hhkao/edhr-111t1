3b5a27db5586139631a4a070159b4a139e86a331
hhkao <heyshoboy@gmail.com>
2022-12-08 11:45:37 +0800

    完成flag84

--- edhr-111t1-run-人事及教務.R
+++ edhr-111t1-run-人事及教務.R
@@ -25,6 +25,11 @@ data_staff   <- read_excel(path, sheet = "職員(工)資料表")
 #data_courseB  <- read_excel(path, sheet = "授課資料表B.無課程代碼（23碼）")
 data_retire   <- read_excel(path, sheet = "離退教職員(工)資料表")
 
+#上一期人事資料檔
+drev_person_pre <- readxl :: read_excel("\\\\192.168.110.244\\share_tmp\\給修恒\\1111\\edhr-111t1\\dta\\edhr_111t1-202210\\1101公立學校及1102私立學校_人事資料整合.xlsx") %>%
+  subset(substr(organization_id, 3, 3) != "1") %>%
+  rename(source = dta_teacher)
+
 # 合併人事資料表 ----------------------------------------------------------------
 data_teacher <- data_teacher %>%
   mutate(source = 1)
@@ -1337,6 +1342,16 @@ drev_person_1 <- drev_person_1 %>%
 
 # 人事資料表與離退教職員(工)資料表合併 -----------------------------------------------------------
 drev_P_retire <- merge(x = drev_person_1, y = data_retire, by = c("organization_id", "idnumber"), all.x = TRUE, all.y = TRUE)
+
+# 前一期人事資料表與離退教職員(工)資料表合併 -----------------------------------------------------------
+drev_P_retire_pre <- merge(x = drev_person_pre, y = data_retire, by = c("organization_id", "idnumber"), all.x = TRUE, all.y = TRUE)
+
+#學校名稱
+edu_name2 <- data.frame(
+  "organization_id" = drev_person$organization_id, 
+  "edu_name2" = drev_person$edu_name2
+) %>%
+  distinct(organization_id, .keep_all = TRUE)
 
 # 人事資料表與教學資料表合併 -----------------------------------------------------------
 drev_P_load <- merge(x = drev_person_1, y = data_load, by = c("organization_id", "idnumber"), all.x = TRUE, all.y = TRUE)
@@ -6925,4 +6940,72 @@ if('flag92' %in% ls()){
     distinct(organization_id, .keep_all = TRUE) %>%
     subset(select = c(organization_id, edu_name2))
   flag92$flag92 <- ""
+}
+
+# flag84: 離退教職員（工）資料表所列人員，應為上一學年（期）專任教職員（工）。 -------------------------------------------------------------------
+flag_person <- drev_P_retire_pre %>%
+  rename(name = name.x, name_retire = name.y) %>%
+  subset(!is.na(flag_person$name_retire)) %>%
+  left_join(edu_name2, by = c("organization_id"))
+
+#填寫在「離退教職員(工)資料表」之人員，聘任類別需為「專任」。(與上一期資料比對)
+flag_person$err_flag <- 0
+flag_person$err_flag <- if_else(flag_person$emptype != "專任", 1, flag_person$err_flag)
+
+#呈現姓名
+flag_person$err_flag_txt <- ""
+flag_person$err_flag_txt <- case_when(
+  flag_person$err_flag == 1 ~ flag_person$name,
+  TRUE ~ flag_person$err_flag_txt
+)
+
+#根據organization_id + source，展開成寬資料(wide)
+flag_person_wide_flag84 <- flag_person %>%
+  subset(select = c(organization_id, idnumber, err_flag_txt, edu_name2, source, err_flag)) %>%
+  subset(err_flag == 1) %>%
+  dcast(organization_id + source ~ err_flag_txt, value.var = "err_flag_txt")
+
+#合併所有name
+temp <- colnames(flag_person_wide_flag84)[3 : length(colnames(flag_person_wide_flag84))]
+flag_person_wide_flag84$flag84_r <- NA
+for (i in temp){
+  flag_person_wide_flag84$flag84_r <- paste(flag_person_wide_flag84$flag84_r, flag_person_wide_flag84[[i]], sep = ", ")
+}
+flag_person_wide_flag84$flag84_r <- gsub("NA, ", replacement="", flag_person_wide_flag84$flag84_r)
+flag_person_wide_flag84$flag84_r <- gsub(", NA", replacement="", flag_person_wide_flag84$flag84_r)
+
+#產生檢誤報告文字
+flag84_temp <- flag_person_wide_flag84 %>%
+  group_by(organization_id) %>%
+  mutate(flag84_txt = paste(source, "：", flag84_r, sep = ""), "") %>%
+  subset(select = c(organization_id, flag84_txt)) %>%
+  distinct(organization_id, flag84_txt)
+
+#根據organization_id，展開成寬資料(wide)
+flag84 <- flag84_temp %>%
+  dcast(organization_id ~ flag84_txt, value.var = "flag84_txt")
+
+#合併教員資料表及職員(工)資料表報告文字
+temp <- colnames(flag84)[2 : length(colnames(flag84))]
+flag84$flag84 <- NA
+for (i in temp){
+  flag84$flag84 <- paste(flag84$flag84, flag84[[i]], sep = "； ")
+}
+flag84$flag84 <- gsub("NA； ", replacement="", flag84$flag84)
+flag84$flag84 <- gsub("； NA", replacement="", flag84$flag84)
+
+#產生檢誤報告文字
+flag84 <- flag84 %>%
+  subset(select = c(organization_id, flag84)) %>%
+  distinct(organization_id, flag84) %>%
+  mutate(flag84 = paste(flag84, "（查貴校上一學年所填資料，上述人員聘任類別非屬『專任』。依欄位說明，非專任教職員(工)之退休或離職者，不須填列離退教職員(工)資料表，請務必再確認。）", sep = ""))
+
+#偵測flag84是否存在。若不存在，則產生NA行
+if('flag84' %in% ls()){
+  print("flag84")
+}else{
+  flag84 <- drev_person_1 %>%
+    distinct(organization_id, .keep_all = TRUE) %>%
+    subset(select = c(organization_id, edu_name2))
+  flag84$flag84 <- ""
 }
